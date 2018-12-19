11b722d08176455ad9a3572cf0bdc8a2a2c6c87c
Jeff Wisard <jwisard@cas.org>
2018-12-19 14:49:34 -0500

    added RetrieveFlavors and RetrieveImages

--- goos.go
+++ goos.go
@@ -3,6 +3,8 @@ package goos
 import (
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/flavors"
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/images"
 	logger "github.com/sirupsen/logrus"
 )
 
@@ -33,4 +35,61 @@ func CreateProviderClient(authConfig *AuthConfig) (*gophercloud.ProviderClient, 
 	}
 
 	return authClient, err
+}
+
+// RetrieveFlavors lists all of the flavors of the project currently indicated by the given ProviderClient
+func RetrieveFlavors(provider *gophercloud.ProviderClient) ([]flavors.Flavor, error) {
+
+	compute, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+		Region: "RegionOne",
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	listOpts := flavors.ListOpts{
+		Limit:      20,
+		AccessType: flavors.PublicAccess,
+	}
+
+	allPages, err := flavors.ListDetail(compute, listOpts).AllPages()
+	if err != nil {
+		return nil, err
+	}
+
+	allFlavors, err := flavors.ExtractFlavors(allPages)
+	if err != nil {
+		return nil, err
+	}
+
+	return allFlavors, nil
+}
+
+// RetrieveImages lists all of the images of the project currently indicated by the given ProviderClient
+func RetrieveImages(provider *gophercloud.ProviderClient) ([]images.Image, error) {
+
+	compute, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+		Region: "RegionOne",
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	listOpts := images.ListOpts{
+		Limit: 20,
+	}
+
+	allPages, err := images.ListDetail(compute, listOpts).AllPages()
+	if err != nil {
+		return nil, err
+	}
+
+	allImages, err := images.ExtractImages(allPages)
+	if err != nil {
+		return nil, err
+	}
+
+	return allImages, nil
 }
